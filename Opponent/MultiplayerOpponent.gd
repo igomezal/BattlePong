e823fa1b84943bafe386fc5df934831918d1d20e
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2020-11-15 20:41:28 +0100

    fix: adjust animation

--- Opponent/MultiplayerOpponent.gd
+++ Opponent/MultiplayerOpponent.gd
@@ -5,5 +5,5 @@ var next_position := Vector2.ZERO
 onready var tween = $Tween
 
 func update_state():
-	tween.interpolate_property(self, "global_position", global_position, next_position, 0.1)
+	tween.interpolate_property(self, "global_position", global_position, next_position, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
 	tween.start()
